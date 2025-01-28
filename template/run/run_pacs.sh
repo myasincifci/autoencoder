2d1d9436977d1336f064f4e638437c92ee86f93f
Yasin Cluster <cifci@campus.tu-berlin.de>
2025-01-28 18:24:51 +0100

    run file

--- template/run/run_pacs.sh
+++ template/run/run_pacs.sh
@@ -5,11 +5,7 @@
 #SBATCH --ntasks-per-node=4
 #SBATCH --output=logs/job-%j.out
 
-# 1. copy the squashed dataset to the nodes /tmp 
-rsync -ah --progress /home/myasincifci/dispatch_smol/data/PACS.hdf5 /tmp
-
-ls /tmp
 
 apptainer run --nv \
     /home/myasincifci/containers/main/main.sif \
-    python ./train.py --config-name base
+    python ./train.py --config-name vaelstm_moving_mnist
